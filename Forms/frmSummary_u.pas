9ec17a727d9667063839e9522dd8aeea15bce8e0
Bob Mannino <manninobobby@icloud.com>
2025-06-24 11:50:17 +0100

    ui: show incomes on summary

--- Forms/frmSummary_u.pas
+++ Forms/frmSummary_u.pas
@@ -27,9 +27,11 @@ type
   private
     { Private declarations }
     Tns: TTransactionsPlus;
-    procedure UpdateExpenses;
-    procedure SetupExpenses;
+    procedure RefreshTransactions;
     procedure SetupExpensesHeaders;
+    procedure SetupIncomesHeaders;
+    procedure RefreshExpenses;
+    procedure RefreshIncomes;
   public
     { Public declarations }
   end;
@@ -47,6 +49,8 @@ var
 begin
   fFrm := TfrmCategories.Create(Self);
   fFrm.ShowModal;
+
+  RefreshTransactions;
 end;
 
 procedure TfrmSummary.btnNewClick(Sender: TObject);
@@ -56,19 +60,28 @@ begin
   fFrm := TfrmNew.Create(Self);
   fFrm.ShowModal;
 
+  RefreshTransactions;
 end;
 
 procedure TfrmSummary.FormCreate(Sender: TObject);
 begin
-  Tns := dmDB.GetTransactions;
-
-  SetupExpenses;
+  RefreshTransactions;
 end;
 
-procedure TfrmSummary.SetupExpenses;
+procedure TfrmSummary.RefreshTransactions;
+begin
+  Tns := dmDB.GetTransactions;
+
+  RefreshExpenses;
+  RefreshIncomes;
+end;
+
+procedure TfrmSummary.RefreshExpenses;
 var
   Epns: TTransactionsPlus;
 begin
+  lblExpenses.Text := 'Expenses';
+
   if Length(Tns) = 0 then
     Exit;
 
@@ -140,14 +153,86 @@ begin
   strGrdExpenses.AddObject(fCol);
 end;
 
-procedure TfrmSummary.UpdateExpenses;
+procedure TfrmSummary.RefreshIncomes;
+var
+  Epns: TTransactionsPlus;
 begin
-  Tns := dmDB.GetTransactions;
+  lblIncomes.Text := 'Incomes';
 
-  SetupExpenses;
+  if Length(Tns) = 0 then
+    Exit;
+
+  for var i := 0 to High(Tns) do
+  begin
+    if Tns[i].CatType = TCategoryType.Income then
+    begin
+      SetLength(Epns, Length(Epns) + 1);
+      Epns[High(Epns)] := Tns[i];
+    end;
+  end;
+
+  lblIncomes.Text := Format('Incomes (%d)', [Length(Epns)]);
+
+  if Length(Epns) = 0 then
+    Exit;
+
+  strGrdIncomes.BeginUpdate;
+  try
+    strGrdIncomes.ClearColumns;
+    strGrdIncomes.RowCount := 0;
+
+    if strGrdIncomes.ColumnCount = 0 then
+      SetupIncomesHeaders;
+
+    strGrdIncomes.RowCount := Length(Epns);
+
+    for var i := 0 to High(Epns) do
+    begin
+      strGrdIncomes.Cells[0, i] := Epns[i].Title;
+      strGrdIncomes.Cells[1, i] := Epns[i].Amount.ToString;
+      strGrdIncomes.Cells[2, i] := Epns[i].FreqQuantity.ToString;
+      strGrdIncomes.Cells[3, i] := TTransaction.StringifyFreqUnit
+        (Epns[i].FreqUnit);
+      strGrdIncomes.Cells[4, i] := Epns[i].CatTitle;
+    end;
+  finally
+    strGrdIncomes.EndUpdate;
+  end;
+end;
+
+procedure TfrmSummary.SetupIncomesHeaders;
+var
+  fCol: TStringColumn;
+begin
+  fCol := TStringColumn.Create(strGrdIncomes);
+  fCol.Header := 'Title';
+  fCol.Width := 150;
+  strGrdIncomes.AddObject(fCol);
+
+  fCol := TStringColumn.Create(strGrdIncomes);
+  fCol.Header := 'Amount';
+  fCol.Width := 75;
+  strGrdIncomes.AddObject(fCol);
+
+  fCol := TStringColumn.Create(strGrdIncomes);
+  fCol.Header := 'Per';
+  fCol.Width := 50;
+  strGrdIncomes.AddObject(fCol);
+
+  fCol := TStringColumn.Create(strGrdIncomes);
+  fCol.Header := 'Unit';
+  fCol.Width := 75;
+  strGrdIncomes.AddObject(fCol);
+
+  fCol := TStringColumn.Create(strGrdIncomes);
+  fCol.Header := 'Category';
+  fCol.Width := 150;
+  strGrdIncomes.AddObject(fCol);
 end;
 
 { TODO : export option }
 { TODO : import option }
+{ TODO : edit transaction }
+{ TODO : remove transaction }
 
 end.
